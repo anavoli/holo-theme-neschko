119cfb862161ed8bfdbafa97705d41eb2f8e6208
ana <105382108+anavoli@users.noreply.github.com>
2024-04-10 20:55:28 +0200

    Update testimonial.js

--- src/components/Home/testimonial.js
+++ src/components/Home/testimonial.js
@@ -8,7 +8,7 @@ const Testimonial = () => {
         <div className="w-full h-full  flex flex-row justify-between align-middle bg-[url('../images/holographic-background.webp')] p-12 rounded-lg xxs:flex-col xs:flex-col sm:flex-row xxs:p-5 lg:p-12">
           <div className="w-3/4 xxs:w-full xs:w-full backdrop-blur-sm bg-white/30 p-10 rounded-lg lg:p-10">
             <div className="text-black font-semibold text-xl xxs:text-s xxs:font-normal sm:text-xl font-notoserif">
-              "Lorem Ipsum is simply dummy text of the printing and typesetting
+              "Lorem Ipsum ++++++++++++++++++++11111111111111++++++++is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book."
